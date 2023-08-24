ffce60a6bac7114524fe654939f30436a3784fbf
BrianSayen <blu3fire89@gmail.com>
2023-08-24 14:39:37 +0800

    update projects

--- src/components/ParallaxProj.tsx
+++ src/components/ParallaxProj.tsx
@@ -1,5 +1,5 @@
-import GithubIcon from "../assets/icons/github.svg";
-import LinkIcon from "../assets/icons/link.svg";
+import GithubIcon from '../assets/icons/github.svg';
+import LinkIcon from '../assets/icons/link.svg';
 
 interface ProductCardProps {
   id: number;
@@ -19,13 +19,13 @@ const ProjectCard = ({ id, data }: ProductCardProps) => {
   return (
     <div
       className={`flex items-center flex-col md:gap-0 gap-4 ${
-        id % 2 >= 1 ? "md:flex-row-reverse" : "md:flex-row"
+        id % 2 >= 1 ? 'md:flex-row-reverse' : 'md:flex-row'
       }`}
     >
-      <div className="drop-shadow-md basis-2/3 w-full h-screen bg-gray-600 items-center flex">
+      <div className='drop-shadow-md basis-2/3 w-full h-screen bg-gray-600 items-center flex'>
         <div
-          className={`image-bg bg-left md:h-screen h-[50vh] md:bg-auto bg-cover ${
-            id % 2 >= 1 ? "md:bg-right" : "bg-left"
+          className={`image-bg md:h-screen h-[50vh] md:bg-auto bg-cover bg-center ${
+            id % 2 >= 1 ? 'md:bg-right' : 'bg-left'
           }`}
           style={{
             backgroundImage: `url(${
@@ -35,33 +35,33 @@ const ProjectCard = ({ id, data }: ProductCardProps) => {
         ></div>
       </div>
       <div
-        id="project-text"
-        className="flex flex-col flex-wrap md:py-10 pb-10 basis-1/3 px-4"
+        id='project-text'
+        className='flex flex-col flex-wrap md:py-10 pb-10 basis-1/3 px-4'
       >
-        <h4 className="uppercase text-sm text-violet-primary tracking-widest">
+        <h4 className='uppercase text-sm text-violet-primary tracking-widest'>
           Featured
         </h4>
-        <h2 className="title pb-8">{data.name}</h2>
+        <h2 className='title pb-8'>{data.name}</h2>
         <p>{data.description}</p>
-        <div className="mt-auto pt-6">
-          <ul className="flex flex-row gap-2">
+        <div className='mt-auto pt-6'>
+          <ul className='flex flex-row gap-2'>
             {data.techStack.map((tech) => {
               return (
-                <li key={tech} className="text-sm">
+                <li key={tech} className='text-sm'>
                   {tech}
                 </li>
               );
             })}
           </ul>
-          <ul className="flex flex-row gap-2">
-            <span className="p-1 cursor-pointer">
-              <a href={data.github} target="_blank">
-                <img className="icon" src={GithubIcon} alt="github-icon" />
+          <ul className='flex flex-row gap-2'>
+            <span className='p-1 cursor-pointer'>
+              <a href={data.github} target='_blank'>
+                <img className='icon' src={GithubIcon} alt='github-icon' />
               </a>
             </span>
-            <span className="p-1 cursor-pointer">
-              <a href={data.website} target="_blank">
-                <img className="icon" src={LinkIcon} alt="link-icon" />
+            <span className='p-1 cursor-pointer'>
+              <a href={data.website} target='_blank'>
+                <img className='icon' src={LinkIcon} alt='link-icon' />
               </a>
             </span>
           </ul>
