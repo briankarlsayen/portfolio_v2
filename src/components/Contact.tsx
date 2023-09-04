74b08db9dae3b3dbca9018d6f2d5447fe728f441
BrianSayen <blu3fire89@gmail.com>
2023-09-04 17:00:58 +0800

    Add tooltip

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,57 +1,78 @@
-import { useState } from "react";
-import GithubIcon from "../assets/icons/github.svg";
-import LinkedinIcon from "../assets/icons/linkedin.svg";
-import InstagramIcon from "../assets/icons/insta.svg";
+import { useState } from 'react';
+import GithubIcon from '../assets/icons/github.svg';
+import LinkedinIcon from '../assets/icons/linkedin.svg';
+import InstagramIcon from '../assets/icons/insta.svg';
+import DevToIcon from '../assets/icons/devto.png';
+import { Tooltip } from 'react-tooltip';
 
 const Contact = () => {
-  const [inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState('');
+  const socialList = [
+    {
+      name: 'Github',
+      label: 'github-icon',
+      link: 'https://github.com/briankarlsayen',
+      img: GithubIcon,
+    },
+    {
+      name: 'Linkedin',
+      label: 'linkedin-icon',
+      link: 'https://linkedin.com/in/briankarlsayen',
+      img: LinkedinIcon,
+    },
+    {
+      name: 'Instagram',
+      label: 'instagram-icon',
+      link: 'https://www.instagram.com/briansayen/',
+      img: InstagramIcon,
+    },
+    {
+      name: 'Devto',
+      label: 'devto-icon',
+      link: 'https://dev.to/blu3fire89',
+      img: DevToIcon,
+    },
+  ];
+
   return (
-    <section id="contact" className="bg-tertiary">
-      <div className="min-h-screen max-w-2xl flex flex-col mx-auto justify-center text-white x-spacing md:px-0">
-        <h4 className="heading tracking-widest text-violet-primary">Contact</h4>
-        <h2 className="title pb-6 hero-title sub-heading">Get in touch</h2>
+    <section id='contact' className='bg-tertiary'>
+      <div className='min-h-screen max-w-2xl flex flex-col mx-auto justify-center text-white x-spacing md:px-0'>
+        <h4 className='heading tracking-widest text-violet-primary'>Contact</h4>
+        <h2 className='title pb-6 hero-title sub-heading'>Get in touch</h2>
         <p>If you want to connect with me, leave your email here</p>
         <p>and I will contact you ASAP.</p>
-        <form className="flex gap-2 pt-6" onSubmit={() => console.log("henlo")}>
+        <form className='flex gap-2 pt-6' onSubmit={() => console.log('henlo')}>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-black-custom outline-none"
-            type="email"
-            placeholder="email"
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-black-custom outline-none'
+            type='email'
+            placeholder='email'
             value={inputText}
             onChange={(e) => setInputText(e.target.value)}
           />
-          <button className="py-2 px-6 text-center bg-quaternary rounded">
+          <button className='py-2 px-6 text-center bg-quaternary rounded'>
             Send
           </button>
         </form>
-        <div className="flex gap-4 pt-4">
-          <span>
-            <a href="https://github.com/briankarlsayen" target="_blank">
-              <img
-                className="cursor-pointer icon-white"
-                src={GithubIcon}
-                alt="github-icon"
-              />
-            </a>
-          </span>
-          <span>
-            <a href="https://linkedin.com/in/briankarlsayen" target="_blank">
-              <img
-                className="cursor-pointer icon-white"
-                src={LinkedinIcon}
-                alt="linkedin-icon"
-              />
-            </a>
-          </span>
-          <span className="relative flex flex-col items-center group">
-            <a href="https://www.instagram.com/briansayen/" target="_blank">
-              <img
-                className="cursor-pointer icon-white"
-                src={InstagramIcon}
-                alt="instagram-icon"
-              />
-            </a>
-          </span>
+        <div className='flex gap-4 pt-4'>
+          {socialList.map((soc) => (
+            <span key={soc.name}>
+              <a href={soc.link} target='_blank'>
+                <img
+                  className='cursor-pointer icon-white h-[24px]'
+                  src={soc.img}
+                  alt={soc.label}
+                  data-tooltip-id={soc.name}
+                  data-tooltip-content={soc.name}
+                />
+                <Tooltip
+                  style={{ background: '#7289DA', color: 'white' }}
+                  noArrow
+                  place='bottom'
+                  id={soc.name}
+                />
+              </a>
+            </span>
+          ))}
         </div>
       </div>
     </section>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,45 @@
 const Navbar = () => {
+  const navList = [
+    {
+      name: 'About',
+      link: '/#about',
+    },
+    {
+      name: 'Project',
+      link: '/#project',
+    },
+    {
+      name: 'Contact',
+      link: '/#contact',
+    },
+    {
+      name: 'Blog',
+      link: 'https://dev.to/blu3fire89',
+      isUrl: true,
+      newPage: true,
+    },
+  ];
+
   return (
     <nav
-      id="navbar"
-      className="flex h-[80px] justify-between items-center x-spacing xl:px-20 bg-primary"
+      id='navbar'
+      className='flex h-[80px] justify-between items-center x-spacing xl:px-20 bg-primary'
     >
-      <span className="font-bold cursor-pointer">brian</span>
-      <ul className="flex gap-4">
-        <li
-          className="cursor-pointer hover:text-[#7289DA]"
-          onClick={() => window.location.replace("/#about")}
-        >
-          About
-        </li>
-        <li
-          className="cursor-pointer hover:text-[#7289DA]"
-          onClick={() => window.location.replace("/#project")}
-        >
-          Project
-        </li>
-        <li
-          className="cursor-pointer hover:text-[#7289DA]"
-          onClick={() => window.location.replace("/#contact")}
-        >
-          Contact
-        </li>
+      <span className='font-bold cursor-pointer'>brian</span>
+      <ul className='flex gap-4'>
+        {navList.map((nav) => (
+          <li
+            key={nav.name}
+            className='cursor-pointer hover:text-[#7289DA]'
+            onClick={() => {
+              nav.isUrl
+                ? window.open(nav.link, '_blank')
+                : window.location.replace(nav.link);
+            }}
+          >
+            {nav.name}
+          </li>
+        ))}
       </ul>
     </nav>
   );
